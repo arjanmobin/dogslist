c41e60211b118a15f5ecc44c08145b2fb3a00709
arjanmobin <arjanmobin@gmail.com>
2018-04-11 12:49:41 -0700

    init

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const hbs = require("hbs");
+
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", function(req, res){
+   res.send("GET/");
+});
+
+
+
+app.listen(port, function(){
+    console.log("Serber running");
+});
